9d6a81c705db2932ffacd88aaf0ceb0ea84a0ff8
Nygen <nducminh002@gmail.com>
2023-04-09 16:01:42 +0200

    Add: password, hooks, options to User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,46 @@
 import mongoose, { mongo } from "mongoose";
-import { prop,getModelForClass } from "@typegoose/typegoose";
-
-export class User{
-    public object: string = "user"
-
-    @prop({required: true})
-    public name!: string
-
-  
-}   
+import {
+  prop,
+  getModelForClass,
+  pre,
+  index,
+  modelOptions,
+} from "@typegoose/typegoose";
+import bcrypt from "bcryptjs";
 
 
-export const UserModel = getModelForClass(User)
+@index({ email: 1 })
+@pre<User>("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 12);
+})
+@modelOptions({
+  schemaOptions: {
+    timestamps: true,
+  },
+})
+export class User {
+  @prop({ required: false, default: "user" })
+  object: string;
+
+  @prop({ required: true, trim: true })
+  name!: string;
+
+  @prop({ required: true, trim: true, unique: true })
+  email!: string;
+
+  @prop({
+    required: true,
+    trim: true,
+    minlength: 8,
+    maxLength: 32,
+    select: false,
+  })
+  password!: string;
+
+  async comparePasswords(hashedPassword: string, comparedPassword: string) {
+    return await bcrypt.compare(comparedPassword, hashedPassword);
+  }
+}
+
+export const UserModel = getModelForClass(User);
