1a03802100dde9c8fcb4242ba5f7f4e6903f1456
Nygen <nducminh002@gmail.com>
2023-04-10 17:33:43 +0200

    Change: json instead of send method

--- src/routes/api/users.route.ts
+++ src/routes/api/users.route.ts
@@ -30,7 +30,7 @@ router.get("/", async function (req: SearchRequest, res) {
   const users: User[] = await UserModel.find({}).limit(
     req.limit || general.listPerPage
   );
-  res.status(200).send(users);
+  res.status(200).json(users);
 });
 router.post(
   "/",
@@ -40,9 +40,9 @@ router.post(
 
     try {
       const created = await UserModel.create(req.body);
-      res.send(created).status(200);
+      res.json(created).status(200);
     } catch (error) {
-      res.send(error).status(400);
+      res.json(error).status(400);
     }
   }
 );
@@ -51,9 +51,9 @@ router.get("/:id", async function (req: Request, res, next) {
   
   try {
     const user = await UserModel.findById(req.params.id);
-    res.send(user).status(200);
+    res.json(user).status(200);
   } catch (error) {
-    res.status(404).send(error);
+    res.status(404).json(error);
   }
 });
 router.delete("/:id", async function (req: UserRequest, res) {
@@ -61,7 +61,7 @@ router.delete("/:id", async function (req: UserRequest, res) {
     const deleted = await UserModel.findByIdAndDelete(req.params.id);
     res.json(deleted).status(204);
   } catch (error) {
-    res.status(400).send({ message: error });
+    res.status(400).json( error );
   }
 });
 router.put("/:id", async function (req: UserRequest, res) {
@@ -69,9 +69,9 @@ router.put("/:id", async function (req: UserRequest, res) {
     const { id } = req.params;
     await UserModel.updateOne({ id }, req.body);
     const updated: User | null = await UserModel.findById(id);
-    res.status(200).send(updated);
+    res.status(200).json(updated);
   } catch (error) {
-    res.json(error)
+    res.json(error).status(400)
   }
 });
 router.get("/search", async function (req: SearchRequest, res) {});
