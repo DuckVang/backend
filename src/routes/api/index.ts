8887081a41426ac9a2be95dc526e36ef805f31d6
Nygen <nducminh002@gmail.com>
2023-04-10 13:22:53 +0200

    Delete: tags route

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -2,13 +2,12 @@ import { Router } from "express";
 import { userRouter } from "./users.route";
 import { productsRouter } from "./services.route";
 import { profilesRouter } from "./profiles.route";
-import { tagsRouter } from "./tags.route";
+
 import { nextTick } from "process";
 
 const router = Router();
 
 router.use("/users", userRouter);
-router.use("/tags", tagsRouter);
 router.use("/products", productsRouter);
 
 
